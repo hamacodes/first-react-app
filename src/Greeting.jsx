80bbc2f13dcd0d5e29695ac17a7e92025f9c9326
Hama Pashazadeh <pashazad@msu.edu>
2025-01-02 14:31:32 -0500

    Update Greeting component to include an image and render it in the main application

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,6 +1,11 @@
 
 function Greeting() {
-    return <h1>&quot; I think, therefore I am.&quot;</h1>;
+    return (
+    <div>
+        <img src="https://www.worldhistory.org/img/c/p/1200x627/12757.jpg" alt="An oil painting of philosopher René Descartes." />
+        <h1>&quot; I think, therefore I am.&quot;</h1>
+    </div>
+    );
 }
 
 export default Greeting;
