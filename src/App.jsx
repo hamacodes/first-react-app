5fed45bd49e42d4b785a4d4f6de3c96cf64552e9
Hama Pashazadeh <pashazad@msu.edu>
2025-01-03 21:03:55 -0500

    Refactor App component to display a list of animals and remove Greeting component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  
-  return (
-    <>
-      <h1>Test title</h1>
-      <svg>
-        <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
-      </svg>
-      <form>
-        <input type="text" />
-      </form>
-    </>
-  )
+function ListItem(props) {
+  return <li>{props.animal}</li>
 }
+
+function List(props) {
+  return (
+    <ul>
+      {props.animals.map((animal) => {
+        return <ListItem key={animal} animal={animal} />;
+      })}
+    </ul>
+  );
+}
+
+function App() {
+  const animals = ["Lion", "Cow", "Snake", "Lizard"];
+
+  return (
+    <div>
+      <h1>Animals: </h1>
+      <List animals={animals} />
+    </div>
+  );
+}
+
 
 export default App
