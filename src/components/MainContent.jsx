10d6fe4a86bf6bf2c3522f4741d02ebc2108ac4b
Hama Pashazadeh <pashazad@msu.edu>
2025-01-06 18:47:17 -0500

    Enhance MainContent component by adding class names for improved styling and maintainability

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,13 +1,13 @@
 function MainContent() {
     return (
       <main>
-          <h1>Fun facts about React</h1>
-          <ol>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on GitHub</li>
-            <li>Is maintained by Meta</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
+          <h1 className="main-h1">Fun facts about React</h1>
+          <ol className="main-ol">
+            <li className="main-li">Was first released in 2013</li>
+            <li className="main-li">Was originally created by Jordan Walke</li>
+            <li className="main-li">Has well over 100K stars on GitHub</li>
+            <li className="main-li">Is maintained by Meta</li>
+            <li className="main-li">Powers thousands of enterprise apps, including mobile apps</li>
           </ol>
       </main>
     )
