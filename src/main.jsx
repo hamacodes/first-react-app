0fc9bf7b39bcc62e068013a919bfa2a432489bda
Hama Pashazadeh <pashazad@msu.edu>
2025-01-05 21:57:20 -0500

    Remove App component rendering from main entry file

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 
 
+
+
+
+
+
+
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+
   </StrictMode>,
 )
