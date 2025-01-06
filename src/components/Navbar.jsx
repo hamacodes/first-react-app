f911230869cd7a0c6d3545cd88d70e4ea3e0c27c
Hama Pashazadeh <pashazad@msu.edu>
2025-01-06 18:43:52 -0500

    Refactor Navbar component by removing navigation menu and adding header text for improved clarity and presentation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,7 @@ function Navbar() {
     return (
       <header className='header'>
           <img className='header-image' src="src/assets/react.svg" alt="React logo" />
-          <nav>
-            <ul className='nav-list'>
-              <li>Pricing</li>
-              <li>About</li> 
-              <li>Contact</li>
-            </ul>
-          </nav>
+          <text className="header-text">ReactFacts</text>
       </header> 
     );
 }
